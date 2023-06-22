7b0b1abd425433ea235d896247d4bba7695f7ae4
sebastianhampel1 <hampel.m@northeastern.edu>
2023-06-22 19:12:19 -0400

    LIGHTING AND INPUT ACHIEVED

---
+++ scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class PlayerMovement : Node3D
+{
+	[Export]
+	private float speed= 5;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector3 position = Position;
+		if (Input.IsActionPressed("ui_right"))
+		{
+			position.X+=speed*(float)delta;
+		}
+		if(Input.IsActionPressed("ui_left")){
+			position.X-=speed*(float)delta;
+		}
+		if(Input.IsActionPressed("ui_up")){
+			position.Z-=speed*(float)delta;
+		}
+		if(Input.IsActionPressed("ui_down")){
+			position.Z+=speed*(float)delta;
+		}
+		Position = position;
+	}
+}
