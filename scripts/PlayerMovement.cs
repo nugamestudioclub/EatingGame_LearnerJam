7db6169e6ca53fd1b29a9b0501888d1fafb394d5
sudzoof <sunjoshua2@gmail.com>
2023-06-26 15:52:55 -0400

    Add Player scene
    Also redid movement logic

--- scripts/PlayerMovement.cs
+++ scripts/PlayerMovement.cs
@@ -1,35 +1,27 @@
 using Godot;
 using System;
 
-public partial class PlayerMovement : Node3D
+public partial class PlayerMovement : CharacterBody3D
 {
-    [Export]
-    private float speed = 5;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
+	[Export]
+	private float speed = 5;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        Vector3 position = Position;
-        if (Input.IsActionPressed("ui_right"))
-        {
-            position.X += speed * (float)delta;
-        }
-        if (Input.IsActionPressed("ui_left"))
-        {
-            position.X -= speed * (float)delta;
-        }
-        if (Input.IsActionPressed("ui_up"))
-        {
-            position.Z -= speed * (float)delta;
-        }
-        if (Input.IsActionPressed("ui_down"))
-        {
-            position.Z += speed * (float)delta;
-        }
-        Position = position;
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		velocity.X = direction.X * speed;
+		velocity.Z = direction.Z * speed;
+		velocity.Y = 0;
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 }
