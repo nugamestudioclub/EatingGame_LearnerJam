eae352b698feb86a96c0bb2bbd126c5dc91c62e5
Sebastian Kobori <sjkobori@gmail.com>
2023-06-26 23:11:05 -0400

    models imported

--- SlimeAnimationPlayer.cs
+++ SlimeAnimationPlayer.cs
@@ -3,10 +3,11 @@ using System;
 
 public partial class SlimeAnimationPlayer : AnimationPlayer
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
-	}
+		Autoplay = "Jamming";
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
